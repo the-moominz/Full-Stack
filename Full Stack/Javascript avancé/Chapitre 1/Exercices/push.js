06d68c0f1d229ea81b719ae77f7d60ce13bbdf11
Elodie666 <bousser.elodie@laposte.net>
2020-09-23 18:18:01 +0200

    miam miam miam

--- Full Stack/Javascript avancé/Chapitre 1/Exercices/push.js
+++
@@ -1,9 +0,0 @@
-// Soient les données suivantes créez un tableau strNumbers et pushez chacune des valeurs de manière individuelle dans ce tableau :
-const strNumbers = [];
-const str1 = ['one', 'two'];
-const str2 = ['three', 'four'];
-
-strNumbers.push(...str1, ...str2); 
-
-
-console.log(strNumbers);
--- Full Stack/Javascript avancé/Chapitre 1/Exercices/ttc.js
+++
@@ -1,7 +0,0 @@
-// Outil calcul ttc
-function ttc (ht, tva = 0.2) {
-    
-    return ht + (tva * ht);
-
-  }
-console.log(ttc(50));
