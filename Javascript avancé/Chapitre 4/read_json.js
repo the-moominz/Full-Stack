2b740e47d7da968b28b475e7a53a1fefc5286627
Elodie666 <bousser.elodie@laposte.net>
2020-09-24 15:32:32 +0200

    encapsulation

--- Javascript avancé/Chapitre 4/read_json.js
+++ Javascript avancé/Chapitre 4/read_json.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
-
-fs.readFile('./data/dragons.json', { encoding: 'utf8' }, (err, data) => {
+/*fs.readFile('./data/dragons.json', { encoding: 'utf8' }, (err, data) => {
     // impossible de lire le fichier
     if (err) {
         console.log("File read failed:", err);
@@ -9,9 +7,29 @@ fs.readFile('./data/dragons.json', { encoding: 'utf8' }, (err, data) => {
     // success
     // JSON.parse permet de transformer un fichier JSON en un objet JSON JS
     console.log('File data:', JSON.parse(data)) 
-})
+})*/
+
+const fs = require('fs');
 
 // 1- Changez le code ci-dessus et utilisez une promesse pour gérez la récupération des données
+const file = filename => ( new Promise((resolve, reject) => {
+    setTimeout(() => {
+        fs.readFile(filename, { encoding: 'utf8' }, (err, data) => {
+            // impossible de lire le fichier
+            if (err) {
+                reject(new Error("File read failed:"));
+
+                return;
+            }
+            resolve(JSON.parse(data));
+        })
+    } ,300);
+    })
+);
+
+file('./data/dragons.json')
+.then(fs => console.log(fs))
+.catch(err => console.error(err));
 
 // 2- Quel est le dragon le plus agé ? Faite un script pour répondre à cette question.
 
