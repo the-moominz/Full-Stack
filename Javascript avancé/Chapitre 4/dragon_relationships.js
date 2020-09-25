ca64b3770ca40238507a898fa27130978f2f0f1d
Elodie666 <bousser.elodie@laposte.net>
2020-09-25 13:56:11 +0200

    Final version

--- Javascript avancé/Chapitre 4/dragon_relationships.js
+++ Javascript avancé/Chapitre 4/dragon_relationships.js
@@ -15,17 +15,32 @@ const readJSON = fileName => {
     );
 }
 
-Promise.all([ readJSON('./data/dragons.json') , readJSON('./data/relationships.json') ])
-    .then( data => {
+Promise.all([readJSON('./data/dragons.json'), readJSON('./data/relationships.json')])
+    .then(data => {
 
         const { dragons } = data[0];
-        const { relationship } = data[1];
+        const { relationships } = data[1];
 
-        let relations = relationship.map(r => r.relation);
-
-        console.log(relations);
         //console.log(dragons);
-        //console.log(relationship);
+        //console.log(relationships);
 
+        let dragonRelations = [];
+        for (relationship of relationships) {
 
+            const dragon = dragons.filter(dragon => dragon.id === relationship.id); //filter renvoie un tableau
+            const { name } = dragon[0];
+
+            const relations = [];
+            const { relation } = relationship;
+            //console.log(relation);
+            for (const id of relation) {
+
+                const { name: dragonName } = dragons.filter(dragon => dragon.id === id)[0];
+                relations.push(dragonName);
+
+            }
+            dragonRelations.push({ name, relations });
+            //console.log(relationship);
+        }
+        console.log(dragonRelations);
     })
